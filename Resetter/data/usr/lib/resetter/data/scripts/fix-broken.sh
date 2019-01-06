b8fa521f1df5543f2b3ae45150a2fa9b14b7e28b
gaining <gaining7@outlook.com>
2019-01-06 18:55:24 -0500

    removed uneeded folder

--- Resetter/data/usr/lib/resetter/data/scripts/fix-broken.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#fix broken packages after package removals by resetter
-rm -f /var/cache/apt/archives/lock
-rm -f /var/lib/dpkg/lock
-dpkg --configure -a
-apt install -fy
-apt autoremove -y
-apt clean
